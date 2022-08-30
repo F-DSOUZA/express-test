6093c18072b71feec67e52d90723a6e50b878509
Francesca DSouza <fran.dsouza.consultant@ft.com>
2022-08-30 20:00:47 +0100

    remote repo added

---
+++ myapp/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+//router.get('/', function(req, res, next) {
+//  let todaysDate = format(new Date(), 'dd/mm/yy');
+//  res.render('index', { title: 'Today\'s Date:', date: todaysDate });
+//});
+//
+//module.exports = router;
